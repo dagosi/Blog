8605c72df8269644725df2a71a0e4fd617dbc336
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-10-28 00:22:44 -0400

    Added dependency to tags on destroy the post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 5 }
 
   has_many :comments, dependent: :destroy
-  has_many :tags
+  has_many :tags, dependent: :destroy
   
   accepts_nested_attributes_for :tags, allow_destroy: :true,
   reject_if: proc { |attrs| attrs.all? { |k,v| v.blank? } }
