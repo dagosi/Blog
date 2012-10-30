dee1c5d13a3881120e42852ed612a646675a4fe4
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-10-30 11:21:50 -0400

    Added styles to pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 5)
+    @posts = Post.paginate(page: params[:page], per_page: 5).order('created_at DESC')
 
     respond_to do |format|
       format.html # index.html.erb
