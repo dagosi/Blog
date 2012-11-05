7ea103c8e26887c56d89d3d16dfd93c19f27cea7
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-11-05 03:13:53 -0500

    Added validation to body in comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   attr_accessible :body, :commenter
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
   validates :commenter, presence: true
 
 end
